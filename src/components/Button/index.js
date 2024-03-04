2de59f07f3d8d1700d23a58d069369bf28010b1f
quan <tranbaminhquan20003@gmail.com>
2024-03-05 01:23:57 +0700

    proptypes

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,5 @@
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-
 import classNames from 'classnames/bind';
 import styles from './Button.module.scss';
 const cx = classNames.bind(styles);
