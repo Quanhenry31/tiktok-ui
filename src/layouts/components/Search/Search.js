4b850d525aacbd98c1fedf2ffd9cbefa75eacae9
quan <tranbaminhquan20003@gmail.com>
2024-03-06 22:39:55 +0700

    Giải quyết vấn đề nhiều file index.js

---
+++ src/layouts/components/Search/Search.js
@@ -0,0 +1,102 @@
+import { useEffect, useState, useRef } from 'react';
+import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import HeadlessTippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+
+import * as searchService from '~/services/searchService';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountItem from '~/components/AccountItem';
+import { SearchIcon } from '~/components/Icons';
+import { useDebounce } from '~/hooks';
+import styles from './Search.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Seach() {
+  //lay du lieu ra // so sanh va dua dl vao
+  const [searchValue, setSearchValue] = useState('');
+  const [searchResult, setSearchResult] = useState([]);
+  const [showResult, setShowResult] = useState(true);
+  const [loading, setloading] = useState(false);
+
+  const debounced = useDebounce(searchValue, 500);
+
+  const inputRef = useRef();
+
+  useEffect(() => {
+    if (!debounced.trim()) {
+      setSearchResult([]);
+      return;
+    }
+
+    const fetchApi = async () => {
+      setloading(true);
+
+      const result = await searchService.search(debounced);
+      setSearchResult(result);
+
+      setloading(false);
+    };
+    fetchApi();
+  }, [debounced]);
+
+  const handleclear = () => {
+    setSearchValue('');
+    inputRef.current.focus();
+    setSearchResult([]);
+  };
+
+  const handleHideResult = () => {
+    setShowResult(false);
+  };
+
+  const handleChange = (e) => {
+    const searchValue = e.target.value;
+    if (!searchValue.startsWith(' ')) {
+      setSearchValue(searchValue);
+    }
+  };
+  return (
+    <div>
+      <HeadlessTippy
+        interactive
+        visible={showResult && searchResult.length > 0}
+        render={(attrs) => (
+          <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+            <PopperWrapper>
+              <h4 className={cx('search-title')}>Accounts</h4>
+              {searchResult.map((result) => (
+                <AccountItem key={result.id} data={result} />
+              ))}
+            </PopperWrapper>
+          </div>
+        )}
+        onClickOutside={handleHideResult}
+      >
+        <div className={cx('search')}>
+          <input
+            ref={inputRef}
+            value={searchValue}
+            placeholder="Seach accounts and videos"
+            spellCheck={false}
+            onChange={handleChange}
+            onFocus={() => setShowResult(true)}
+          ></input>
+          {!!searchValue && !loading && (
+            <button className={cx('clear')} onClick={handleclear}>
+              <FontAwesomeIcon icon={faCircleXmark} />
+            </button>
+          )}
+          {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+
+          <button className={cx('search-btn')} onMouseDown={(e) => e.preventDefault()}>
+            <SearchIcon />
+          </button>
+        </div>
+      </HeadlessTippy>
+    </div>
+  );
+}
+
+export default Seach;
